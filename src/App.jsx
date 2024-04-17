fd3eb0d67a9d364fdb3887e8c4d5ed811967d166
George-Gabechava <108359813+George-Gabechava@users.noreply.github.com>
2024-04-17 08:45:37 -0400

    getting data from API, but it's not going into my list

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,63 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import jikanGet from './jikanAPI'
 
 function App() {
   // const [count, setCount] = useState(0)
-  jikanGet(12, 0);
+  let list = [
+    {
+      name: "Alphonse Elric",
+      id: 12,
+      url: "placeholder"
+    },
+    {
+      name: "Erwin Smith",
+      id: 46496,
+      url: "placeholder"
+    },
+    // {
+    //   name: "Satoru Gojou",
+    //   id: 164471,
+    //   url: ""
+    // }
+  ]
 
+  // populate the url field for each character
+  //might need to useEffect/useState here
+  for (let i = 0; i < list.length; i++) {
+    console.log("get pictures urls for", list[i].name);
+    useEffect(() => {
+      async function startFetching() {
+        const result = await jikanGet(list[i].id);
+        if (!ignore) {
+          console.log(i, list[i].url);
+          list[i].url = result;
+          console.log(i, list[i].url);
+        }
+      }
+
+      let ignore = false;
+      startFetching();
+      return () => {
+        ignore = true;
+      }
+    }, []);
+  }  
+
+  const renderCards = list.map(character =>
+    <div className='card' key={character.id}>
+      {character.name}
+      <br></br>
+      {character.id}
+      <br></br>
+      {character.url}
+      {/* image from url here */}
+      </div>
+  );
+  
   return (
     <>
-      hello
+     {renderCards}
     </>
   )
 }
--- src/jikanAPI.jsx
+++ src/jikanAPI.jsx
@@ -5,39 +5,21 @@ import { useState, useEffect } from 'react'
 function jikanGet(characterID, pictureIndex) {
   // Call format: https://api.jikan.moe/v4/characters/{ID}/pictures where ID is found from https://myanimelist.net/character/{ID}
   // GET request using fetch with error handling
-  console.clear();
-
-  let currentComponent;
   let url = 'https://api.jikan.moe/v4/characters/' + characterID + '/pictures'
-  console.log(url);
   fetch(url)
-  .then(async response => {
-      const dataList = await response.json();
-      console.log(dataList.data.length)
-      console.log(response)
+    .then(response => {
       // check for error response
-      if (response.statusText !== "OK") {
-          // get error message from body or default to response statusText
-          const error = (data && data.message) || response.statusText;
-          return Promise.reject(error);
+      if (response.statusText !== "OK" || response.status !== 200) {
+        const error = (response.statusText || response.status);
+        return error;
       }
-
-      console.log("we made it")
-      currentComponent.setState({ data })
-  })
-
-
-//// work on error cathcing
-  .catch(error => {
-      currentComponent.setState({ errorMessage: error.toString() });
-      console.error('There was an error!', error);
-  });
-
-  console.log(currentComponent);
-  return (
-    currentComponent
-    // return the image  
-  )
+      response = response.json();
+      return response;
+    }).then(response => {
+      console.log("jikanGet", response.data);
+      // to return first image: use response.data[0].jpg.image_url
+      return response.data;
+    });
 }
 
 export default jikanGet
